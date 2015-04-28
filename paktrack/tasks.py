b08d3e57845db5faf912d3ce1006f4200c33ef12
Saravana Kumar <saran87@gmail.com>
2015-04-28 09:34:25 -0400

    - fixing vibration library import for ubuntu environment

--- paktrack/tasks.py
+++ paktrack/tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from celery.utils.log import get_task_logger
 from paktrack.celery import app
 
-from paktrack.vibration import Vibration
+from paktrack.vibration import vibration
 import logging.config
 import logging
 
@@ -14,7 +14,7 @@ logger = get_task_logger(__name__)
 
 @app.task
 def process_data(db_host, db_port, db, truck_id, package_id):
-    vib = Vibration.Vibration(db_host, db_port, db)
+    vib = vibration.Vibration(db_host, db_port, db)
     return vib.process_data(truck_id,package_id)
 
 
